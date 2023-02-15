a0ba850346d4042908c1b656344838315dcf1d46
vin-ll <hegewaldvin@gmail.com>
2023-02-15 22:47:26 +0100

    Uploading files

---
+++ src/core/scan_ip.rs
@@ -0,0 +1,91 @@
+use std::{net::TcpStream};
+
+pub fn scan(ip: &str) {
+    let port_range: Vec<u16> = (1..65535).collect();
+
+    let mut handles = vec![];
+
+    let mut open_ports = Vec::new();
+    let total_ports = port_range.len();
+    let mut completed_ports = 0;
+
+    let start_time = std::time::Instant::now();
+
+    for port in port_range {
+        let ip = ip.to_string();
+        let handle = std::thread::spawn(move || {
+            let connection_with_timeout = TcpStream::connect_timeout(
+                &format!("{}:{}", ip, port).parse().unwrap(),
+                std::time::Duration::from_secs(1),
+            );
+            match connection_with_timeout {
+                Ok(_) => {
+                    return Some(port);
+                }
+                Err(_) => {
+                    return None;
+                }
+            }
+        });
+        handles.push(handle);
+        completed_ports += 1;
+
+        if handles.len() == 1000 {
+            for handle in handles {
+                if let Some(port) = handle.join().unwrap() {
+                    open_ports.push(port);
+                }
+            }
+            handles = vec![];
+        }
+
+        if completed_ports % 100 == 0 {
+            let percentage = (completed_ports as f64 / total_ports as f64) * 100.0;
+            let elapsed_time = start_time.elapsed();
+            let time_per_port = elapsed_time.as_secs_f64() / (completed_ports as f64);
+            let remaining_ports = total_ports - completed_ports;
+            let remaining_time_secs = (time_per_port * remaining_ports as f64) as u64;
+            let remaining_time_mins = remaining_time_secs / 60;
+
+            print!("\x1B[2J\x1B[1;1H");
+
+            println!("Port sniffer CLI by https://github.com/vin-ll | repository: https://github.com/vin-ll/port-sniffer-cli");
+            println!("{}% complete. Estimated time remaining: {} secs ({} mins)", percentage, remaining_time_secs, remaining_time_mins);
+        }
+    }
+
+    for handle in handles {
+        if let Some(port) = handle.join().unwrap() {
+            open_ports.push(port);
+        }
+    }
+
+    let elapsed_time = start_time.elapsed();
+    println!("Scan completed in {}m {}s", elapsed_time.as_secs() / 60, elapsed_time.as_secs() % 60);
+
+    if open_ports.len() == 0 {
+        println!("No open ports found.");
+    } else {
+        println!("PORT\tSERVICE\tPROTOCOL");
+        for port in open_ports {
+            let service = match port {
+                21 => "FTP",
+                22 => "SSH",
+                23 => "Telnet",
+                25 => "SMTP",
+                53 => "DNS",
+                80 => "HTTP",
+                110 => "POP3",
+                143 => "IMAP",
+                443 => "HTTPS",
+                993 => "IMAPS",
+                995 => "POP3S",
+                3306 => "MySQL",
+                8080 => "HTTP",
+                _ => "Unknown",
+            };
+            println!("{}\t{}\t{}", port.to_string(), service, "TCP");
+        }
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod core;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect::<Vec<String>>();
+
+    if args.len() == 1 {
+        eprintln!("Error: You need to specify a command to run. Run -h or --help for more information.");
+        std::process::exit(1);
+    } 
+
+    let command: String = args[1].clone();
+
+    if command == "-h" || command == "--help" {
+        println!("Usage: {} [command] [arguments]", args[0]);
+        println!(" -h, --help\tDisplay this help message");
+        println!(" -v, --version\tDisplay the version of this program");
+        println!(" -s, --scan\tScan an IP address. Usage: -s [IPv4 Address]");
+    } else if command == "-v" || command == "--version" {
+        let version: String = env!("CARGO_PKG_VERSION").to_string();
+        println!("Version: {}", version);
+    } else if command == "-s" || command == "--scan" {
+        if args.len() == 2 {
+            eprintln!("Error: You need to specify an IP address to scan. Run -h or --help for more information.");
+            std::process::exit(1);
+        }
+
+        let ip: String = args[2].clone();
+        core::scan_ip::scan(&ip);
+    } else {
+        eprintln!("Error: Unknown command. Run -h or --help for more information.");
+    }
+
+    std::process::exit(0);
+}
